a4c70083ab8a0a66db355d3a18c60a0cdb75892c
Timofey Fomin <94967340+ShiroyashaSG@users.noreply.github.com>
2024-10-12 16:00:22 +0300

    Develop (#27)
    Внесены изменения в модели Review, Comment pub_date c auto_now_add,
    заменена на default, так как иначе в импорте данных будут проблеммы,
    данные с установленной датой заменятся на текущую дату. Изменена
    валидация для поля year модели Title. Поле descriptions модели Title
    теперь null=False. Переписан блок импорта данных в БД. Regex перенесены
    в константы. Для модели User заменен класс Role с Enum на
    models.TextChoices.

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,5 +1,5 @@
 import os
-import pandas
+import csv
 
 from django.conf import settings
 from django.core.management import BaseCommand
@@ -12,146 +12,61 @@ class Command(BaseCommand):
     """Команда для импорта данных из csv."""
 
     def handle(self, *args, **kwargs):
-        self.import_genres()
-        self.import_categories()
-        self.import_titles()
-        self.import_users()
-        self.import_reviews()
-        self.import_comments()
-        self.import_genre_title()
+        self.import_data(Genre, 'genre')
+        self.import_data(Category, 'category')
+        self.import_data(User, 'users')
+        self.import_data(Title, 'titles')
+        self.import_data(Review, 'review')
+        self.import_data(Comment, 'comments')
+        self.import_genre_title('genre_title')
 
-    def import_genres(self):
-        data_path = os.path.join(settings.BASE_DIR, 'static/data/genre.csv')
-        df = pandas.read_csv(data_path)
-        for _, row in df.iterrows():
-            genre, created = Genre.objects.get_or_create(
-                name=row['name'],
-                slug=row['slug']
-            )
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(f'Жанр "{genre.name}" создан.')
-                )
-            else:
-                self.stdout.write(f'Жанр "{genre.name}" уже существует.')
-
-    def import_categories(self, *args, **kwargs):
-        data_path = os.path.join(settings.BASE_DIR, 'static/data/category.csv')
-        df = pandas.read_csv(data_path)
-        for _, row in df.iterrows():
-            category, created = Category.objects.get_or_create(
-                id=row['id'],
-                name=row['name'],
-                slug=row['slug']
-            )
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(f'Категория "{category.name}" создана.')
-                )
-            else:
-                self.stdout.write(
-                    f'Категория "{category.name}" уже существует'
-                )
-
-    def import_titles(self):
-        data_path = os.path.join(settings.BASE_DIR, 'static/data/titles.csv')
-        df = pandas.read_csv(data_path)
-
-        for _, row in df.iterrows():
-            category_id = row['category']
-            category = Category.objects.get(id=category_id)
-            title, created = Title.objects.get_or_create(
-                name=row['name'],
-                year=row['year'],
-                category=category
-            )
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(f'Название "{title.name}" создано.')
-                )
-            else:
-                self.stdout.write(
-                    f'Название "{title.name}" уже существует.'
-                )
-
-    def import_users(self):
-        data_path = os.path.join(settings.BASE_DIR, 'static/data/users.csv')
-        df = pandas.read_csv(data_path)
-        for _, row in df.iterrows():
-            user, created = User.objects.get_or_create(
-                id=row['id'],
-                username=row['username'],
-                email=row['email'],
-                defaults={
-                    'role': row.get('role', 'user'),
-                    'bio': row.get('bio', ''),
-                    'first_name': row.get('first_name', ''),
-                    'last_name': row.get('last_name', ''),
-                }
-            )
-            if created:
-                self.stdout.write(self.style.SUCCESS(
-                    f'Пользователь "{user.username}" создан.')
-                )
-            else:
-                self.stdout.write(
-                    f'Пользователь "{user.username}" уже существует.'
-                )
-
-    def import_reviews(self):
-        data_path = os.path.join(settings.BASE_DIR, 'static/data/review.csv')
-        df = pandas.read_csv(data_path)
-        for _, row in df.iterrows():
-            author_id = row['author']
-            user = User.objects.get(id=author_id)
-            review, created = Review.objects.get_or_create(
-                title_id=row['title_id'],
-                text=row['text'],
-                author=user,
-                score=row['score'],
-                pub_date=row['pub_date']
-            )
-            if created:
-                self.stdout.write(self.style.SUCCESS(
-                    f'Отзыв создан для названия с ID "{row["title_id"]}".')
-                )
-            else:
-                self.stdout.write(
-                    'Отзыв уже существует для'
-                    + f'названия с ID "{row["title_id"]}".'
-                )
-
-    def import_comments(self):
-        data_path = os.path.join(settings.BASE_DIR, 'static/data/comments.csv')
-        df = pandas.read_csv(data_path)
-        for _, row in df.iterrows():
-            author_id = row['author']
-            user = User.objects.get(id=author_id)
-            comment, created = Comment.objects.get_or_create(
-                review_id=row['review_id'],
-                text=row['text'],
-                author=user,
-                pub_date=row['pub_date']
-            )
-            if created:
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        'Комментарий создан для'
-                        + f'отзыва с ID "{row["review_id"]}".'
-                    )
-                )
-            else:
-                self.stdout.write(
-                    'Комментарий уже существует для'
-                    + f'отзыва с ID "{row["review_id"]}".'
-                )
-
-    def import_genre_title(self):
-        data_path = os.path.join(
-            settings.BASE_DIR, 'static/data/genre_title.csv'
+    @staticmethod
+    def read_file(file_name):
+        """Чтение данных из csv файла для последующего импорта."""
+        file_path = os.path.join(
+            settings.BASE_DIR, 'static/data/', f'{file_name}.csv'
         )
-        df = pandas.read_csv(data_path)
-        for _, row in df.iterrows():
+        with open(file_path, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            return list(reader)
+
+    def get_new_data(self, cur_model, file_name):
+        """Загружаем только те данные, которых нет в БД."""
+        reader = self.read_file(file_name)
+        db_data = list(cur_model.objects.all().values(*reader[0].keys()))
+        for record in db_data:
+            for key in record.keys():
+                record[key] = str(record[key])
+        db_data_set = {tuple(d.items()) for d in db_data}
+        new_data = [
+            row for row in reader if tuple(row.items()) not in db_data_set
+        ]
+        return new_data
+
+    def import_data(self, cur_model, file_name):
+        """Импорт данных в БД."""
+        reader = self.get_new_data(cur_model, file_name)
+        for row in reader:
+            if cur_model == Title:
+                category_id = row.pop('category', None)
+                row['category'] = Category.objects.get(id=category_id)
+            elif cur_model in (Review, Comment):
+                author_id = row.pop('author', None)
+                row['author'] = User.objects.get(id=author_id)
+            model, created = cur_model.objects.get_or_create(**row)
+            if created:
+                self.stdout.write(
+                    self.style.SUCCESS(f'Добавлена запись: {model}')
+                )
+            else:
+                self.stdout.write(
+                    self.style.WARNING(f'Запись уже существует: {model}')
+                )
+
+    def import_genre_title(self, file_name):
+        """Импорт данных в БД для связи многие ко многим."""
+        reader = self.read_file(file_name)
+        for row in reader:
             # Проверяем существование title и genre перед созданием связи
             try:
                 title = Title.objects.get(id=row['title_id'])
